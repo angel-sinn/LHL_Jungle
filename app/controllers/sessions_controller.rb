85af21d04d5a5ebeaef5ff4ba2edc7a8d27137f2
Angel Sinn <angelkysinn@gmail.com>
2021-04-01 18:35:29 +0000

    Renamed file to sessions_controller from session_controller. Updated to SessionsController instead of SessionController.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+# create (login) and destroy (logout) sessions
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    # if user exists and password entered is correct
+    if user && user.authenticate(params[:password])
+      # save user id inside browser cookie - how we keep user logged in when they navigate around website
+      session[:user_id] = user.id
+      redirect_to '/'
+    else 
+      # if user's login doesn't work, send them back to login form
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+
+end
