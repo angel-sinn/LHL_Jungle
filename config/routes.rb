13dd83e694b3834e060a97d2cbc1f9cfb0d4505b
Angel Sinn <angelkysinn@gmail.com>
2021-04-01 01:42:58 +0000

    Implemented route, controller, v, and views for new admin categories page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :about, only: [:index]
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -15,7 +16,9 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
+    resources :categories, only: [:index, :new, :create]
     resources :products, except: [:edit, :update, :show]
+
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
