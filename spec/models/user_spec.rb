6d798f80d029dedcab8b690101a98a38e00788e1
Angel Sinn <angelkysinn@gmail.com>
2021-04-06 00:04:53 +0000

    Added name, email and password validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,18 +4,54 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
 
-    context 'given a user first name, last name, email and password that is not blank with matching password confirmation' do
+    context 'given a user name, email and password that is not blank with matching password confirmation' do
       it 'is a valid user' do
-        @user = User.new(name: "Angel", email: "angel@test.com", password: "password", password_confirmation: "password")
+        @user = User.create(name: "Angel", email: "angel@test.com", password: "password", password_confirmation: "password")
         expect(@user).to be_valid
+      end
+    end
+
+    context 'given no user name' do
+      it 'is not a valid user' do
+        @user = User.create(name: nil, email: "angel@test.com", password: "password", password_confirmation: "password")
+        expect(@user).to_not be_valid
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+      end
+    end
+
+    context 'given no user email' do
+      it 'is not a valid user' do
+        @user = User.create(name: "Angel", email: nil, password: "password", password_confirmation: "password")
+        expect(@user).to_not be_valid
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+    end
+
+    context 'given an email that already exists in the database, regardless of case' do
+      it 'is not a valid user' do
+        @user1 = User.create(name: "Angel", email: "angel@test.com", password: "password", password_confirmation: "password")
+        @user2 = User.create(name: "Angel", email: "angel@test.com", password: "password", password_confirmation: "password")
+        @user3 = User.create(name: "Angel", email: "ANGEL@test.com", password: "password", password_confirmation: "password")
+        expect(@user2).to_not be_valid
+        expect(@user2.errors.full_messages).to include("Email has already been taken")
+        expect(@user3).to_not be_valid
+        expect(@user3.errors.full_messages).to include("Email has already been taken")
       end
     end
 
     context 'given the password and password confirmation do not match' do
       it 'is not a valid user' do
-        @user = User.new(name: "Angel", email: "angel@test.com", password: "password", password_confirmation: "password1234")
+        @user = User.create(name: "Angel", email: "angel@test.com", password: "password", password_confirmation: "password1234")
         expect(@user).to_not be_valid
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+    end
+
+    context 'given a password with less than 6 characters in length' do
+      it 'is not a valid user' do
+        @user = User.create(name: "Angel", email: "angel@test.com", password: "pass", password_confirmation: "pass")
+        expect(@user).to_not be_valid
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
     end
 
